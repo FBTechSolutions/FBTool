56099af96125e7639ca9714bbea2d1ed76e217f2
jcupe <jcupe@zagile.com>
2023-03-20 22:27:52 -0500

    BM: Adding BM directory

--- src/main/java/ic/unicamp/bm/cli/util/msg/InfoMessages.java
+++ src/main/java/ic/unicamp/bm/cli/util/msg/InfoMessages.java
@@ -4,7 +4,7 @@ public interface InfoMessages {
 
     String INF_0__ENDING_FROM_PROMPT = "Closing the prompt";
     String INF_0__BM_ASCII = " FB Tool";
-    String INF_0__SPLM_VERSION = "FB Tool Version: 0.1";
+    String INF_0__SPLM_VERSION = "FB Tool Version: 0.2";
     String INF_0__SPLM_AUTHOR = "Author: Junior Cupe Casquina";
     String INF_0__WELCOME_SPLM = "<< FB Tool -- Console APP >>";
     String INF_0__CMD_ACCEPTED = "<cmd accepted>";
