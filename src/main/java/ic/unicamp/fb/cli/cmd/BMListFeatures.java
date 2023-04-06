95489b40294b36b57b5813e7152550dccc96112e
jcupe <jcupe@zagile.com>
2023-04-06 11:30:28 -0500

    Refactoring code

--- src/main/java/ic/unicamp/fb/cli/cmd/BMListFeatures.java
+++ src/main/java/ic/unicamp/fb/cli/cmd/BMListFeatures.java
@@ -1,8 +1,10 @@
 package ic.unicamp.fb.cli.cmd;
 
 import ic.unicamp.fb.graph.neo4j.schema.Feature;
+import ic.unicamp.fb.graph.neo4j.schema.relations.FeatureToFragment;
 import ic.unicamp.fb.graph.neo4j.services.FeatureService;
 import ic.unicamp.fb.graph.neo4j.services.FeatureServiceImpl;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(
@@ -12,14 +14,23 @@ public class BMListFeatures implements Runnable {
 
     public static final String CMD_NAME = "list-features";
 
+    @CommandLine.Option(names = {"-f", "--fragments"}, description = "Add fragments to the list")
+    private boolean fragmentEnabled;
+
     @Override
     public void run() {
-        System.out.println("Listing all features...");
+        System.out.println("Fetching feature list...");
         FeatureService featureService = new FeatureServiceImpl();
         for (Feature feature : featureService.findAll()) {
             String message = String.format("id: %s label: %s", feature.getFeatureId(),
                     feature.getFeatureLabel());
             System.out.println(message);
+            if (fragmentEnabled) {
+                System.out.println("    fragments:");
+                for (FeatureToFragment relation : feature.getAssociatedTo()) {
+                    System.out.println("        " + relation.getEndFragment().getFragmentId());
+                }
+            }
         }
     }
 }
